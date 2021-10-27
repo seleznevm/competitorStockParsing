7725497eba25a682cee81c5bc1dde241e57388b7
seleznevm <CbybqCbhjg17>
2021-10-27 17:54:21 +0300

    Work PC_27.10.21

--- main.py
+++ main.py
@@ -4,45 +4,33 @@ import pandas as pd
 import re
 from bs4 import BeautifulSoup
 
-MSK = 'Москва'
-SPB = 'Санкт-Петербург'
-NSB = 'Новосибирск'
-global warehouse_MSK
-warehouse_MSK = 0
-warehouse_SPB = 0
-warehouse_NSB = 0
-
 with open("NNZ_IN.htm", encoding="utf8") as f:
     page = BeautifulSoup(f, "lxml")
 
 page_list = page.find_all("div", class_ = "catcard_desc")
 
 # TMP: getting the model name
-model = page_list[0].find_all("meta", itemprop = "model")
-model = re.search("\".+?\"", str(model))
+page_tmp = page_list[0].find_all("meta", itemprop = "model")
+model = re.search("\".+?\"", str(page_tmp))
 model = model.group()
 model = re.sub("\"", "", str(model))
-print(model)
+
+#getting the warehouse name and q'ty
+page_tmp = page_list[0].find("li")
+warehouse = page_tmp.span.children
+for child in warehouse:
+    warehouse = child
+
+qty = page_tmp.b.children
+for child in qty:
+    qty = child
+#TODO: 1. continue to list page_tmp and find warehouses and qties
 
 # TODO: Create the pandas table and insert the values
 try:
     data = pd.read_excel(r'NNZ_moxa_warehouse.xlsx')
 except:
     print("Can't read the file")
-
-
-
-#### enumerate all lines and find the match with patterns ###
-def find_stock(pattern):
-    for i in range(0, len(page_list)):
-        a = re.search(pattern, str(page_list[i]))
-        if str(a) != "None":
-            qty = re.findall('[0-9][0-9]+\+', str(page_list[i])) or re.findall('[0-9]', str(page_list[i]))
-            return qty
-
-# warehouse_MSK = find_stock(MSK)
-# warehouse_SPB = find_stock(SPB)
-# warehouse_NSB = find_stock(NSB)
 
 
 ############## extrenal request ##############
--- test.py
+++ test.py
@@ -1,4 +1,6 @@
 import re
-string = "<meta content=\"EDS-P510A-8PoE-2GTXSFP-T\" itemprop=\"model\"/>"
-model = re.search("\w", string)
+string = '<meta content="EDS-P510A-8PoE-2GTXSFP-T" itemprop="model"/>'
+model = re.search("\".+?\"", string)
+model = model.group()
+model = re.sub("\"", "", str(model))
 print(model)
