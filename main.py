dff70ad75c65dca48e9df9c1046e1d1189d26302
seleznevm <CbybqCbhjg17>
2021-11-03 17:00:52 +0300

    1.1 add ACS links and for loop by them

--- main.py
+++ main.py
@@ -4,6 +4,7 @@ import pandas as pd
 import re
 from bs4 import BeautifulSoup
 
+# ACS links
 NNZ_CONTROLLERS = "https://nnz-ipc.ru/catalogue/automation/controllers/?pa=100"
 NNZ_MODULES = "https://nnz-ipc.ru/catalogue/automation/io/?pa=100"
 NNZ_DAQ_BOARDS = "https://nnz-ipc.ru/catalogue/automation/io_boards/?pa=100"
@@ -14,11 +15,19 @@ NNZ_HMI = "https://nnz-ipc.ru/catalogue/automation/hmi/?pa=100"
 NNZ_SENSORS = "https://nnz-ipc.ru/catalogue/automation/remote_data_logger/?pa=100"
 NNZ_ACCESSORIES = "https://nnz-ipc.ru/catalogue/automation/automation_accesories/?pa=100"
 
+# PromPC links
+NNZ_EMBEDDED = "https://nnz-ipc.ru/catalogue/ipc/embedded_pc/?pa=100"
+NNZ_PANELPC = "https://nnz-ipc.ru/catalogue/ipc/panel_pc/?pa=100"
+NNZ_COMPUTERS = "https://nnz-ipc.ru/catalogue/ipc/computers/?pa=100"
+NNZ_DIGSIGNAGE = "https://nnz-ipc.ru/catalogue/ipc/digital_signage/?pa=100"
+NNZ_CPU_BOARDS = "https://nnz-ipc.ru/catalogue/ipc/processor_boards/?pa=100"
+NNA_PASSIVE_BOARDS = "https://nnz-ipc.ru/catalogue/ipc/backplane/?pa=100"
+
 ACS_list = [NNZ_CONTROLLERS, NNZ_MODULES, NNZ_DAQ_BOARDS, NNZ_EXP_UNITS, NNZ_SPEC_MODELS, NNZ_DB_BOARDS, NNZ_HMI, NNZ_SENSORS, NNZ_ACCESSORIES]
 
 ############## internal request ##############
-with open("NNZ_IN.htm", encoding="utf8") as f:
-    page = BeautifulSoup(f, "lxml")
+# with open("NNZ_IN.htm", encoding="utf8") as f:
+#     page = BeautifulSoup(f, "lxml")
 
 ############## extrenal request ##############
 # url = "https://nnz-ipc.ru/catalogue/comm/ethernet/?pa=300&sort=available"
@@ -27,30 +36,34 @@ with open("NNZ_IN.htm", encoding="utf8") as f:
 
 WH_List = []
 model_list = []
-page_list = page.find_all("div", class_ = "catcard_desc")
-for n in range(0, len(page_list)):
-    # TMP: getting the model name
-    page_model = page_list[n].find_all("meta", itemprop = "model")
-    model = re.search("\".+?\"", str(page_model))
-    model = model.group()
-    model = re.sub("\"", "", str(model)) # here we got a clear model name
-    page_warehouse = page_list[n].find_all("li") # now let's deal with the stock information
-    for w in range(0,len(page_warehouse)):
-        #getting the warehouse name
-        warehouse = page_warehouse[w].span.children  #TODO: 2. think how to pass the EOL models
-        for child in warehouse:
-            warehouse = child # here we got the clear city name of the warehouse
-        qty = page_warehouse[w].b.children
-        for child in qty:
-            qty = child
-        if warehouse == "Замена от производителя":
-            break
-        dict = {"Склад": str(warehouse), "Количество": str(qty)}
-        WH_List.append(dict)
-        model_list.append(model)
+
+for url in ACS_list:
+    r = requests.get(url)
+    page = BeautifulSoup(r.text, "lxml")
+    page_list = page.find_all("div", class_ = "catcard_desc")
+    for n in range(0, len(page_list)):
+        # TMP: getting the model name
+        page_model = page_list[n].find_all("meta", itemprop = "model")
+        model = re.search("\".+?\"", str(page_model))
+        model = model.group()
+        model = re.sub("\"", "", str(model)) # here we got a clear model name
+        page_warehouse = page_list[n].find_all("li") # now let's deal with the stock information
+        for w in range(0,len(page_warehouse)):
+            #getting the warehouse name
+            warehouse = page_warehouse[w].span.children  #TODO: 2. think how to pass the EOL models
+            for child in warehouse:
+                warehouse = child # here we got the clear city name of the warehouse
+            qty = page_warehouse[w].b.children
+            for child in qty:
+                qty = child
+            if warehouse == "Замена от производителя":
+                break
+            dict = {"Склад": str(warehouse), "Количество": str(qty)}
+            WH_List.append(dict)
+            model_list.append(model)
 
 
 df = pd.DataFrame(WH_List, index= model_list, columns= ["Склад", "Количество"], dtype="string")
 df.index.name = "Модель"
-df.to_excel("info2.xlsx")
+df.to_excel("ACS.xlsx")
 print(df)
